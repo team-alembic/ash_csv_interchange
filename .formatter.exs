e2b82d52d9038ec5888c296bd559e971074c7b2d
Rebecca Le <543859+sevenseacat@users.noreply.github.com>
2026-09-19 05:01:51 +0800

    chore: Move formatter config from original app (#6)
    This allows the package to be included as an `import_dep` and the
    `locals_without_parens` will still apply

--- .formatter.exs
+++ .formatter.exs
@@ -1,3 +1,18 @@
+locals_without_parens = [
+  csv_imports: 1,
+  csv_import: 1,
+  csv_import: 2,
+  csv_exports: 1,
+  csv_export: 1,
+  csv_export: 2,
+  label: 1,
+  headers: 1,
+  upsert_action: 1,
+  import_source: 1,
+  read_action: 1,
+  columns: 1
+]
+
 [
   # Import the :ash and :spark formatter rules. This extension is a Spark DSL,
   # and without these `mix format` mangles the `csv_imports`/`csv_exports` DSL
@@ -9,5 +24,9 @@
     # Quokka reads .credo.exs and rewrites based on those rules.
     # Exclude a few rewrites that tend to be noisy in a library context.
     exclude: [:line_length]
+  ],
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
   ]
 ]
